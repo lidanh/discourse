ab8b4b19f09a5f06252b8644b5a16d65f6ea400f
Lidan Hifi <lidanh@wix.com>
2015-06-22 00:48:28 +0300

    change welcome message

--- config/initializers/06-ensure_login_hint.rb
+++ config/initializers/06-ensure_login_hint.rb
@@ -2,7 +2,7 @@
 if User.limit(20).count < 20 && User.where(admin: true).count == 1
   notice =
     if GlobalSetting.developer_emails.blank?
-      "Congratulations, you installed Discourse! Unfortunately, no administrator emails were defined during setup, so finalizing the configuration <a href='https://meta.discourse.org/t/how-to-create-an-administrator-account-after-install/14046'>may be difficult</a>."
+      "Congratulations, you installed the forum successfully! Unfortunately, no administrator emails were defined during setup."
     else
       emails = GlobalSetting.developer_emails.split(",")
       if emails.length > 1
@@ -10,7 +10,7 @@ if User.limit(20).count < 20 && User.where(admin: true).count == 1
       else
         emails = emails[0]
       end
-      "Congratulations, you installed Discourse! Register a new admin account with #{emails} to finalize configuration."
+      "Congratulations, you installed the forum successfully! Register a new admin account with #{emails} to finalize configuration."
     end
 
   if notice != SiteSetting.global_notice
